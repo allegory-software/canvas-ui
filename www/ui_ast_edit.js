c58473c423bad3b7093c4a8ce03c0f7598187e9c
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2026-02-05 02:45:44 +0200

    ast_edit (wip)

--- www/ui_ast_edit.js
+++ www/ui_ast_edit.js
@@ -1,7 +1,41 @@
-
 (function () {
 "use strict"
 const G = window
+
+/*
+
+TODO:
+
+- Lua tokenizer -> tokens array
+- Lua parser -> AST
+	- tabs are separate tokens
+	- newlines are separate tokens
+- line_number -> start_node array
+- tree rendering
+	- syntax highlighting
+	- tab size adjustment
+	- show spaces with dots
+	- matching parens highlight
+	-
+- tree hit-testing
+- line numbers rendering
+- tree editing
+	- cursor movement between tokens
+	- cursor movement within token
+	- insert char (split token)
+	- delete char (merge tokens)
+	- linear selection
+	- node selection
+	- block selection
+	- indent / outdent lines
+	- indent / outdent block
+	- cut, copy, paste
+	- multiple cursors
+- code navigation
+	-
+
+*/
+
 
 let VIEW_ID  = ui.S-1
 let VIEW_AST = ui.S+0
@@ -14,16 +48,23 @@ view.create = function(cmd, id, ast, fr, align, valign, min_w, min_h) {
 	return ui.cmd_box(cmd, fr, align, valign, min_w, min_h, id, ast)
 }
 
-function draw_node(cx, x, y, node) {
+function draw_node(cx, x, y, x0, node) {
 	let s = node.s
 	let m = ui.measure_text(cx, s)
 	let asc = m.fontBoundingBoxAscent
 	let dsc = m.fontBoundingBoxDescent
-	cx.save()
-	cx.font = '22px Arial'
 	cx.fillStyle = 'white'
 	cx.fillText(s, x, y + asc)
-	cx.restore()
+	if (s == '\n') {
+		y += (asc + dsc) * 1.5
+		x = x0
+	} else {
+		x += m.width
+	}
+	if (node.c)
+		for (let cnode of node.c)
+			[x, y] = draw_node(cx, x, y, x0, cnode)
+	return [x, y]
 }
 
 view.draw = function(a, i) {
@@ -39,7 +80,10 @@ view.draw = function(a, i) {
 	cx.fillStyle = 'black'
 	cx.fillRect(x, y, w, h)
 
-	draw_node(cx, x, y, ast.root)
+	cx.save()
+	cx.font = '22px Monospace'
+	draw_node(cx, x, y, x, ast.root)
+	cx.restore()
 
 }
 
@@ -54,6 +98,7 @@ view.hit = function(a, i) {
 ui.box_widget('ast_edit_view', view)
 
 ui.ast_edit = function(...args) {
+	//ui.m(ui.sp() * 10)
 	ui.ast_edit_view(...args)
 }
 
