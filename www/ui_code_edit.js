62f3a19ef2614206fee08fb4149b20d76d5e3ec9
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2026-02-06 17:56:19 +0200

    code_edit (wip)

--- www/ui_code_edit.js
+++ www/ui_code_edit.js
@@ -5,11 +5,14 @@ const G = window
 
 const {
 	cx,
+	BOX_ARGS
 } = ui
 
 let node_colors = {
 	OpenTag: 'yellow',
 }
+
+ui.load_font('mono', 'fonts/jetbrains-mono-nl-regular.woff2')
 
 function indent_n(s) {
 	let i = 0
@@ -37,6 +40,53 @@ function detect_line_terminator(s) {
 	if (lf && !crlf && !cr) return '\n'
 }
 
+let SIDEBAR_SY  = BOX_ARGS+0
+let SIDEBAR_VI1 = BOX_ARGS+1
+let SIDEBAR_VI2 = BOX_ARGS+2
+
+ui.box_widget('code_edit_sidebar', {
+	create: function(cmd, sidebar_w, line_count, line_h, font_size, font_descent) {
+		return ui.cmd_box(cmd, 0, 's', 's', sidebar_w, 0,
+			0, 0, 0,
+			line_count, line_h, font_size, font_descent
+		)
+	},
+	draw: function(a, i) {
+		let x0  = a[i+0]
+		let y0  = a[i+1]
+		let w   = a[i+2]
+		let h   = a[i+3]
+
+		let sy  = a[i+SIDEBAR_SY]
+		let vi1 = a[i+SIDEBAR_VI1]
+		let vi2 = a[i+SIDEBAR_VI2]
+
+		let line_count   = a[i+BOX_ARGS+3]
+		let line_h       = a[i+BOX_ARGS+4]
+		let font_size    = a[i+BOX_ARGS+5]
+		let font_descent = a[i+BOX_ARGS+6]
+
+		cx.save()
+
+		cx.beginPath()
+		cx.rect(x0, y0, w, h)
+		cx.clip()
+
+		cx.translate(w, -sy)
+
+		cx.font = font_size+'px mono'
+		cx.fontKerning = 'none'
+		cx.textAlign = 'right'
+		cx.fillStyle = 'gray'
+
+		for (let i = vi1; i < vi2; i++)
+			cx.fillText(i, x0, y0 + (i + 1) * line_h - font_descent - 2)
+
+		cx.restore()
+
+	},
+})
+
 ui.widget('code_edit_text', {
 	create: function(...args) {
 		return ui.cmd(...args)
@@ -51,29 +101,16 @@ ui.widget('code_edit_text', {
 		let line_h      = a[i+6]
 		let font_size   = a[i+7]
 		let font_descent= a[i+8]
-		let text_x      = a[i+9]
-		let sidebar_gap = a[i+10]
-		let char_w      = a[i+11]
-		let vi1         = a[i+12]
-		let vi2         = a[i+13]
-		let lines       = a[i+14]
-		let line_colors = a[i+15]
-		let hit_line    = a[i+16]
+		let char_w      = a[i+9]
+		let vi1         = a[i+10]
+		let vi2         = a[i+11]
+		let lines       = a[i+12]
+		let line_colors = a[i+13]
+		let hit_line    = a[i+14]
 		cx.save()
 
 		cx.font = font_size+'px mono'
 		cx.fontKerning = 'none'
-
-		// draw selection
-		cx.fillStyle = ui.bg_color('bg1')
-		cx.fillRect(vx, y0 + hit_line * line_h, vw, line_h)
-
-		// draw side bar
-		cx.textAlign = 'right'
-		cx.fillStyle = 'gray'
-		for (let i = vi1; i < vi2; i++) {
-			cx.fillText(i+1, x0 + text_x - sidebar_gap, y0 + (i + 1) * line_h - font_descent - 2)
-		}
 
 		// reset viewport to alpha 0 so we can blend text with highlighting rectangles.
 		cx.clearRect(vx, vy, vw, vh)
@@ -84,7 +121,7 @@ ui.widget('code_edit_text', {
 		for (let i = vi1; i < vi2; i++) {
 			let s = lines[(i-vi1)]
 			let indent_w = indent_n(s) * char_w * 3
-			cx.fillText(s, x0 + text_x + indent_w, y0 + (i + 1) * line_h - font_descent - 2)
+			cx.fillText(s, x0 + indent_w, y0 + (i + 1) * line_h - font_descent - 2)
 		}
 
 		// this blending mode will draw only where alpha != 0, i.e. over the text.
@@ -99,7 +136,7 @@ ui.widget('code_edit_text', {
 				let ci    = c[j+0]
 				let cw    = c[j+1]
 				let color = c[j+2]
-				let x = x0 + text_x + indent_w + ci * char_w
+				let x = x0 + indent_w + ci * char_w
 				let y = y0 + i * line_h
 				let w = cw * char_w
 				let h = line_h
@@ -110,6 +147,10 @@ ui.widget('code_edit_text', {
 
 		// this blending mode will draw only where alpha == 0, i.e. around the text.
 		cx.globalCompositeOperation = 'destination-over'
+
+		// draw selection
+		cx.fillStyle = ui.bg_color('bg1')
+		cx.fillRect(vx, y0 + hit_line * line_h, vw, line_h)
 
 		// draw background.
 		cx.fillStyle = ui.bg_color('bg0')
@@ -130,8 +171,6 @@ function code_edit_view(id, opt) {
 	let line_h
 	let char_w
 	let max_line_len
-	let sidebar_gap
-	let text_x
 	let text_w
 	let text_h
 	let last_vi1 = -1, last_vi2 = -1 // visible line range
@@ -209,13 +248,14 @@ function code_edit_view(id, opt) {
 			} else {
 				line_colors[line1_i].push(i, w, color)
 			}
-			pr(line1_i, line2_i, i, w, color)
 		} while (cursor.next())
 
 		max_line_len = 0
 		for (let s of lines)
 			max_line_len = max(max_line_len, s.length)
 	}
+
+	let sidebar_i
 
 	function on_text_frame(a, _i, x, y, w, h, vx, vy, vw, vh) {
 
@@ -228,6 +268,11 @@ function code_edit_view(id, opt) {
 		let vi2 = vi1 + vn
 		vi1 = max(0, min(vi1, lines.length - 1))
 		vi2 = max(0, min(vi2, lines.length))
+
+		// TODO: make the sidebar a popup anchored to this frame and remove this hack!
+		a[sidebar_i+SIDEBAR_SY ] = sy
+		a[sidebar_i+SIDEBAR_VI1] = vi1
+		a[sidebar_i+SIDEBAR_VI2] = vi2
 
 		if (last_vi1 != vi1 || last_vi2 != vi2) {
 			visible_lines.length = 0
@@ -250,7 +295,7 @@ function code_edit_view(id, opt) {
 
 		ui.stack(id+'.text_contentbox')
 			ui.code_edit_text(x, y, vx, vy, vw, vh,
-				line_h, font_size, font_descent, text_x, sidebar_gap, char_w,
+				line_h, font_size, font_descent, char_w,
 				vi1, vi2, visible_lines, visible_colors,
 				hit_line,
 		)
@@ -269,9 +314,7 @@ function code_edit_view(id, opt) {
 		let m = ui.measure_text(cx, '0')
 		char_w = ceil(m.width)
 		font_descent = m.fontBoundingBoxDescent
-		sidebar_gap = sp
 		let sidebar_w = (lines.length+'').length * char_w
-		text_x = sidebar_w + sidebar_gap
 		text_w = ceil(max_line_len * char_w)
 		text_h = lines.length * line_h
 
@@ -281,11 +324,17 @@ function code_edit_view(id, opt) {
 		ctrl  = ui.key('control')
 
 		ui.v(fr, 0, align, valign, min_w, min_h)
-			ui.h(0)
+			ui.h(1, ui.sp025())
+				ui.p(ui.sp1(), 0)
+				ui.stack('', 0)
+					ui.bb('bg1')
+					sidebar_i = ui.code_edit_sidebar(sidebar_w,
+						lines.length, line_h, font_size, font_descent)
+				ui.end_stack()
+				ui.scrollbox(id+'.text_scrollbox', 1, 'auto', 'scroll')
+					ui.frame(noop, on_text_frame, 0, 'l', 't', text_w, text_h)
+				ui.end_scrollbox()
 			ui.end_h()
-			ui.scrollbox(id+'.text_scrollbox', 1, 'auto', 'scroll', 's', 's')
-				ui.frame(noop, on_text_frame, 0, 'l', 't', text_w, text_h)
-			ui.end_scrollbox()
 		ui.end_v()
 
 	}
